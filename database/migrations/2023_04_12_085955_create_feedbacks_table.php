392fa4dba86ee116d801bdb095e6ebe3e0eec12b
michaelmegally <michaelgamil8@gmail.com>
2023-04-12 12:28:05 +0200

    first commit

---
+++ database/migrations/2023_04_12_085955_create_feedbacks_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('feedbacks', function (Blueprint $table) {
+            $table->id();
+            $table->string('tasklevel')->default('none');
+            $table->integer('duration');
+            $table->integer('rating');
+            $table->longText('comments');
+            $table->string('recommendations');
+            $table->foreignId('user_id')->constrained()->onUpdate('cascade')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('feedbacks');
+    }
+};
---
+++ database/migrations/2023_04_12_092507_create_services_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('services', function (Blueprint $table) {
+            $table->id();
+            $table->string('companyname')->default('unknown');
+            $table->string('service');
+            $table->integer('phone');
+            $table->longText('description')->nullable();
+            $table->enum('paystatus',['cash','card']);
+            $table->integer('annualrevenues');
+            $table->date('date');
+            $table->foreignId('user_id')->constrained()->onUpdate('cascade')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('services');
+    }
+};
