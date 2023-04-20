c352d643b38bab5eed969cce27913babbee7b512
MichaelMegally <michaelgamil8@gmail.com>
2023-04-20 10:57:29 -0700

    home page

---
+++ resources/views/home.blade.php
@@ -0,0 +1,125 @@
+<!
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="{{asset("https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css")}}" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{asset("css/bootstrap.min.css")}}">
+    <link rel="stylesheet" href="{{asset("css/home.css")}}">
+
+    <!-- link Font Awesome -->
+    <link rel="stylesheet" href="{{asset("https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css")}}" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer"/>
+
+    <!-- Animaton on Scroll -->
+    <link rel="stylesheet" href="{{asset("https://unpkg.com/aos@next/dist/aos.css")}}" />
+    <title>home</title>
+</head>
+
+
+<nav class="navbar navbar-expand-lg navbar-light mt-3 mb-2" >
+    <!-- Container wrapper -->
+    <div class="container">
+        <!-- Navbar brand -->
+        <div class="navbar-brand mt-2 mt-lg-0">
+            <!-- <img src="#" alt="Corona Logo"/> -->
+            <h1>DYB</h1>
+        </div>
+
+        <!-- Toggle button -->
+        <button
+            class="navbar-toggler ms-auto"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#nav"
+            aria-controls="nav"
+            aria-expanded="false"
+            aria-label="Toggle navigation">
+            <i class="fas fa-bars"></i>
+        </button>
+
+
+        <div class="collapse navbar-collapse" id="nav">
+            <!-- nav item  -->
+
+            <div class="side-nav  ms-auto">
+
+
+                <ul class="navbar-nav mb-2 mb-lg-0 d-flex">
+
+                    <li class="nav-item">
+                        <a href="#"><i class="fa-solid fa-phone" style="color: black; font-size: 24px;  padding-top: 7px; padding-right: 30px;"></i></a>
+                    </li>
+
+                    <li class="nav-item" >
+                        <a href="#"><i class="fa-solid fa-envelope" style="color: black; font-size: 24px; padding-top: 7px; padding-right: 25px;"></i></a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a class="nav-link active" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" href="#">Our work</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" href="#">Services</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" href="#">Request your service</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" href="#">Contact us</a>
+                    </li>
+                </ul>
+
+            </div>
+
+        </div>
+        <div class="navbar-overlay"></div>
+    </div>
+</nav>
+<div class="container-fluid bg-overlay">
+    <div class="row text-center mt-5">
+        <div class="col background-content">
+
+        <h1 class="text-uppercase" style="font-size: 60px">experience<br> the best digital
+        <br><span class="text-success">agency</span>
+        </h1>
+        <span class="text-capitalize">
+            we help you to achieve your goals by customizing what is best for you<br> with care and productivity.
+        </span><br><br>
+        <a class="btn  text-uppercase">discover
+            <i class="fa-solid fa-arrow-right-long ms-3"></i>
+        </a>
+        </div>
+    </div>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- Animaton on Scroll -->
+<script src="{{asset("https://unpkg.com/aos@next/dist/aos.js")}}"></script>
+<script>AOS.init();</script>
+
+
+<!-- Links Bootsrtap -->
+<script src="{{asset("https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.1/jquery.min.js")}}" integrity="sha512-aVKKRRi/Q/YV+4mjoKBsE4x3H+BkegoM/em46NNlCqNTmUYADjBbeNefNxYV7giUp0VxICtqdrbqU7iVaeZNXA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<script src="{{asset("https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js")}}" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="{{asset("js/bootstrap.bundle.min.js")}}"></script>
+
+<!-- Link Javascript -->
+<script src="{{asset("js/script.js")}}"></script>
+<body>
+
+</body>
+</html>
