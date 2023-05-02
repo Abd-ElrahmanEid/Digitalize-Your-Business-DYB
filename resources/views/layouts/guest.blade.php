93613194c9d9a336871e3e87d560f2388c698501
Abd-ElrahmanEid <eidabdelrahman985@gmail.com>
2023-05-02 17:09:48 +0300

    some changes in pages

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -13,17 +13,21 @@
 
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+
     </head>
     <body class="font-sans text-gray-900 antialiased">
-        <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
+        <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0" style="background-image: url({{'images/Gear.png'}}); background-size: cover;">
             <div>
                 <a href="/">
-                    <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
+                    <x-application-logo class="w-20 h-20 fill-current " style="margin-top: 50px; color: rgba(219, 112, 147)" />
                 </a>
             </div>
 
-            <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
-                {{ $slot }}
+            <div class="w-full sm:max-w-md mt-6 px-4 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg" style="background-color: rgba(219, 112, 147, 0.5);">
+                <div style="background-image: url({{'images/corner.png'}}); background-size: cover; border: 1px solid white; border-radius: 5px" class="px-6 py-4" >
+                    {{ $slot }}
+                </div>
             </div>
         </div>
     </body>
